29d7755b6b5a25b7e134ef301399ddeb46d342bd
Ronnie Brown <ronnievoice@gmail.com>
2015-06-02 22:26:56 -0700

    refactor to use express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,38 +1,50 @@
-/* Import node's http module: */
-var http = require("http");
-var handleRequest = require("./request-handler");
-var port = 3000;
-var ip = "127.0.0.1";
+var express = require('express');
+var app = express();
+var statusCode;
+var results = [];
+//var msg = JSON.stringify( {'results': results} );
 
-var server = http.createServer(handleRequest.requestHandler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10,
+  "Content-Type" : "application/json"
+};
 
+app.get(/classes/, function(req, res) {
+  res.status(200).json( {'results': results} );
+});
+
+app.post(/classes/, function(req, res) {
+  res.status(201).json( {'results': results} );
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http:localhost', host, port);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// /* Import node's http module: */
 // var http = require("http");
 // var handleRequest = require("./request-handler");
-// var url = require('url');
 // var port = 3000;
 // var ip = "127.0.0.1";
 
-// var routes = {
-//   '/classes/chatterbox' : handleRequest,
-//   '/classes/messages' : handleRequest,
-//   '/classes/room' : handleRequest,
-//   '/classes/room1' : handleRequest
-// }
-
-// var server = http.createServer(function(request, response) {
-//   // console.log("Serving request type " + request.method + " for url " + request.url);
-
-//   var parts = urlParser.parse(request.url);
-
-//   var route = routes[parts.pathname];
-//   if ( route ) {
-//     route(request, response);
-//   } else {
-//     utilities.sendResponse(response, "Not Found", 404);
-//   }
-
-// });
+// var server = http.createServer(handleRequest.requestHandler);
 // console.log("Listening on http://" + ip + ":" + port);
 // server.listen(port, ip);
+
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,113 +1,99 @@
-var url = require('url');
-var fs = require('fs');
-var objectId = 1;
-var results = [];
+exports.requestHandler = function() {
 
+app.get('/', function(req, res) {
+  res.send(200, 'Hola')
+});
 
-exports.requestHandler = function(request, response) { 
-  
-  var sendResponse = function(code, body, type) {
-    headers = defaultCorsHeaders;
-    // set default type if undefined
-    headers['Content-Type'] = type || 'text/plain';
-    response.writeHead(code, headers);
-    response.end(body);
-  };
-  
-  // set default status code
-  var statusCode = 404;
-  // set default body message
-  var msg = JSON.stringify( {'results': results} );
-  
-    // handle HTTP requests
-    var pathname = url.parse(request.url).pathname;
-    var method = request.method;
-
-    // 1st path for client, 2nd path for tests
-    if (pathname === '/classes/chatterbox' || pathname === '/classes/messages') {
-      
-      // server logs
-      console.log("Serving request type " + method + " for url " + pathname);
-
-      if (method === 'GET') {
-
-        statusCode = 200;
-        sendResponse(statusCode, msg);
-
-      }     
-   
-      else if (method === 'POST') {
-      
-        var chunks = '';
-        request.on('data', function(chunk) {
-          chunks += chunk;
-        });
-        request.on('end', function(){
-          statusCode = 201;
-          var message = JSON.parse(chunks);
-          // client uses objectId to add a message
-          message.objectId = ++objectId;
-          results.push(message);
-          sendResponse(statusCode, JSON.stringify({'results': results}), 'application/JSON');
-        });
-
-      }
-      
-      else if (method === 'OPTIONS') {
-        statusCode = 200;
-        sendResponse(statusCode, null);
-      }
-
-    }
-    else {
-      msg = 'Non-existent file';
-      sendResponse(statusCode, msg);
-    }
+app.post('/', function(req, res) {
+  res.send(201, 'Post request');
+});
 
 };
 
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
 
 
-// var utilities = require('./utilities');
 
+
+
+
+
+
+
+
+
+
+
+// var url = require('url');
 // var objectId = 1;
-// var messages = [
-//   {
-//     text: "Hello World",
-//     username: "dooks",
-//     objectId: objectId
-//   }
-// ];
+// var results = [];
 
-// var actions = {
-//   'GET' : function(request, response) {
-//     utilities.sendResponse(response, {results : messages});
-//   },
-//   'POST' : function(request, response) {
-//     collectData(request, function(message) {
-//       messages.push(message);
-//       message.objectId = ++objectId;
-//       utilities.sendResponse(response, {objectId: 1});
-//     });
-//   },
-//   'OPTIONS' : function(request, response) {
-//     utilities.sendResponse(response, null);
-//   }
-// }
 
 // exports.requestHandler = function(request, response) { 
-//   var action = actions[request.method];
-//   if ( actions ) {
-//     actions(request, response);
-//   } else {
-//     utilities.sendResponse(response, "Not Found", 404);
-//   }
+  
+//   var sendResponse = function(code, body, type) {
+//     headers = defaultCorsHeaders;
+//     // set default type if undefined
+//     headers['Content-Type'] = type || 'text/plain';
+//     response.writeHead(code, headers);
+//     response.end(body);
+//   };
+  
+//   // set default status code
+//   var statusCode = 404;
+//   // set default body message
+//   var msg = JSON.stringify( {'results': results} );
+  
+//     // handle HTTP requests
+//     var pathname = url.parse(request.url).pathname;
+//     var method = request.method;
+
+//     // 1st path for client, 2nd path for tests
+//     if (pathname === '/classes/chatterbox' || pathname === '/classes/messages') {
+      
+//       // server logs
+//       console.log("Serving request type " + method + " for url " + pathname);
+
+//       if (method === 'GET') {
+
+//         statusCode = 200;
+//         sendResponse(statusCode, msg);
+
+//       }     
+   
+//       else if (method === 'POST') {
+      
+//         var chunks = '';
+//         request.on('data', function(chunk) {
+//           chunks += chunk;
+//         });
+//         request.on('end', function(){
+//           statusCode = 201;
+//           var message = JSON.parse(chunks);
+//           // client uses objectId to add a message
+//           message.objectId = ++objectId;
+//           results.push(message);
+//           sendResponse(statusCode, JSON.stringify({'results': results}), 'application/JSON');
+//         });
+
+//       }
+      
+//       else if (method === 'OPTIONS') {
+//         statusCode = 200;
+//         sendResponse(statusCode, null);
+//       }
+
+//     }
+//     else {
+//       msg = 'Non-existent file';
+//       sendResponse(statusCode, msg);
+//     }
+
 // };
 
+// var defaultCorsHeaders = {
+//   "access-control-allow-origin": "*",
+//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//   "access-control-allow-headers": "content-type, accept",
+//   "access-control-max-age": 10 // Seconds.
+// };
 
